7640e55fddfebfbc64937172716f31e023f78a9d
Igor V <igor@vishenkov.ru>
2023-01-06 22:55:07 +0300

    HW-15.3 First 2023/01/06 22:55

---
+++ 15.3/kms/bucket.tf
@@ -0,0 +1,18 @@
+// Создаём Bucket
+resource "yandex_storage_bucket" "netology-bucket" {
+    access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+    secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+    bucket = "netology-bucket"
+    acl    = "public-read"
+}
+
+// Размещаем картинку в Bucket с правами на чтение всем.
+resource "yandex_storage_object" "pictures" {
+    access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+    secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+    bucket = yandex_storage_bucket.netology-bucket.bucket
+    key = "test_pic.jpg"
+    source = "../img/maxresdefault.jpg"
+    acl    = "public-read"
+    depends_on = [yandex_storage_bucket.netology-bucket]
+}
